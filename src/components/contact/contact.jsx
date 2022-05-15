b6eda2de47df7c1978f8e024a6eb9697e919002d
Zain UL Abdin <bhuttozen@gmail.com>
2022-05-15 19:44:46 +0500

    contact and footer - for now complete

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './contact.css'
 import {MdOutlineMail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
-const contact = () => {
+const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+
+    emailjs.sendForm('service_qpbjq96', 'template_5vh6tzi', form.current, '7vJZxVm-GjH9A2pK1')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -14,35 +32,36 @@ const contact = () => {
         <div className="contact__options">
 
           <article className='contact__option'>
-            <MdOutlineMail />
+            <MdOutlineMail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>bhuttozain1@gmail.com</h5>
             <a href="mailto:bhuttozain1@gmail.com" target='_blank'>Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>bhuttozain1@gmail.com</h5>
             <a href="https://m.me/za.bhutto.351" target='_blank'>Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <a href="https://api.whatsapp.com/send?phone+923146168947" target='_blank'>Send a message</a>
           </article>
         </div>
 
-        <form action="">
+        <form ref={form}  onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Full Name' required />
           <input type='email' name='email' placeholder="Your Email" required />
           <textarea name="message" rows="7" placeholder='Your Message' required />
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
+        
       </div>
     </section>
   )
 }
 
-export default contact
+export default Contact
